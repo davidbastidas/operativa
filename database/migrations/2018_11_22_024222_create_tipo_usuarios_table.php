7f907f1b4896650825778d329620ac532396af38
David <espabastidas19@gmail.com>
2018-11-21 22:28:42 -0500

    models

---
+++ database/migrations/2018_11_22_024222_create_tipo_usuarios_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTipoUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tipo_usuarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tipo_usuarios');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/usuarios', [
+    'as' => 'usuarios', 'uses' => 'UsuariosController@index'
+]);
